899a231a5d1e3602cef50dfa19ebd259b7d36a1f
David A. Desrosiers <setuid@gmail.com>
2020-01-13 11:10:36 -0500

    Updating python_requires and test for Python 3 so this doesn't get installed there.

--- setup.py
+++ setup.py
@@ -9,11 +9,15 @@ except ImportError:
 if 'sdist' in sys.argv:
     os.system('./admin/makedoc')
 
+if not sys.version_info[0] == 2:
+    sys.exit("Sorry, Python 3 is not supported. Please use python3-openid")
+
 version = '[library version:2.2.5]'[17:-1]
 
 setup(
     name='python-openid',
     version=version,
+    python_requires=">2.6.*,<3.0.*",
     description='OpenID support for servers and consumers.',
     long_description='''This is a set of Python packages to support use of
 the OpenID decentralized identity system in your application.  Want to enable
